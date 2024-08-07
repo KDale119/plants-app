f353bdc5b25ee815bd7f5321881318c10ed2c093
Kayla Dale <kayla.dale@mutualofomaha.com>
2024-08-06 19:14:25 -0500

    removed unnecessary extra code

--- src/main/java/com/mccneb/edu/demo/PlantsAppApplication.java
+++ src/main/java/com/mccneb/edu/demo/PlantsAppApplication.java
@@ -2,11 +2,6 @@ package com.mccneb.edu.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PlantsAppApplication {
