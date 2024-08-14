c31b9b28d87403d715822d117c90ac831239d9fd
Evan Stohlmann <estohlmann@gmail.com>
2024-08-14 02:03:39 -0600

    We can create users

--- src/main/java/com/mccneb/edu/demo/controller/UserController.java
+++ src/main/java/com/mccneb/edu/demo/controller/UserController.java
@@ -4,21 +4,14 @@ import com.mccneb.edu.demo.model.Login;
 import com.mccneb.edu.demo.model.User;
 import com.mccneb.edu.demo.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin
 public class UserController {
 
     private final UserService userService;
